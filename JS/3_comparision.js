85843daaeba2be5398ce2689d2da3bd563bcf1b8
viren-69 <virenlande35@gmail.com>
2025-02-05 16:58:44 +0000

    comparision

---
+++ JS/3_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2>=1);
+// console.log(2==1);
+
+// problem comes when you compare 2 different data types.
+
+// console.log(null < 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null <= 0); // true coz comparision converts null into number.
+// console.log(null >= 0);
+
+// ===
+
+console.log("2" === 3);
+
