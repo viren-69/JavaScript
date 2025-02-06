ac01bfe0a7e0aac8d822c4471799371c99cf78a0
viren-69 <virenlande35@gmail.com>
2025-02-06 13:05:19 +0000

    data type summary

--- JS/3_comparision.js
+++ JS/3_comparision.js
@@ -13,6 +13,20 @@
 // console.log(null >= 0);
 
 // ===
-
 console.log("2" === 3);
+
+// Arrays, Objects, Functions
+
+const heros = ["a", "b", "c"];
+
+let myObj = {
+    name: "viren",
+    age: 19,
+
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
 
