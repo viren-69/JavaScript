201cd80efe53192b131946ae7f4d56df6df25cf0
viren-69 <virenlande35@gmail.com>
2025-02-07 17:59:11 +0000

    String

---
+++ JS/4_String.js
@@ -0,0 +1,36 @@
+const name = "viren";
+const age = 19;
+
+console.log(name+ age); // this is out-dataed.
+
+// more popular now.
+console.log(`Hello, my name is ${name} and my age is ${age}`);
+
+const IGname = new String("viren");
+// console.log(IGname[0]);
+// console.log(IGname.toUpperCase());
+// console.log(IGname.length);
+// console.log(IGname.charAt(4));
+// console.log(IGname.indexOf('n'));
+
+
+const Str1 = "     virenn      "
+// console.log(Str1);
+// console.log(Str1.trim());
+
+const Str2 = "hello today is friday."
+// console.log(Str2.split(" "));
+
+
+// console.log(Math);
+// console.log(Math.abs(-10));
+// console.log(Math.round(10.49));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
