2228aa1f74a90e65cd84765767c70a02ee577273
viren-69 <virenlande35@gmail.com>
2025-02-09 07:04:16 +0000

    About Arrays

---
+++ JS/5_Dates.js
@@ -0,0 +1,7 @@
+let myDate = new Date();
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
