b6cf0cffd6f47c4bc3cd8da7e0ee7c52c9813687
viren-69 <virenlande35@gmail.com>
2025-02-10 17:30:55 +0000

    more about functions

--- JS/3_basics/1_functions.js
+++ JS/3_basics/1_functions.js
@@ -4,11 +4,11 @@ function greeting(){
 // greeting();
 
 function sum ( num1, num2){
-    console.log(num1+num2);
+    // console.log(num1+num2);
     
 }
 const result = sum(1,2);
-console.log(result); // it gives undifined because the function is not returning any value.
+// console.log(result); // it gives undifined because the function is not returning any value.
 // sum(1,"2");
 
 function sum ( num1, num2){
@@ -16,4 +16,26 @@ function sum ( num1, num2){
     
 }
 const result2 = sum(1,2);   // now the function will return the value.
-console.log(result2);
+// console.log(result2);
+
+
+function calculateCartPrice(...num1){   // Rest operator(...)
+    return num1;
+}
+// console.log(calculateCartPrice(100,200,500,4738));
+
+// Handling Objects
+const user = {
+    username: "viren",
+    price: 199
+}
+
+function handleObjects(anyObject){
+    console.log(`username is ${anyObject.username} and the price is ${anyObject.price}`);
+}
+
+// handleObjects(user);
+handleObjects({
+    username: "rohan",
+    price: 899
+});
