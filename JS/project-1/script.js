8b77f0bc30b74eb73078bfa3a1a298bc3ae861bc
viren-69 <virenlande35@gmail.com>
2025-02-15 03:19:14 +0000

    changing the backgroungColor project

---
+++ JS/project-1/script.js
@@ -0,0 +1,19 @@
+const btn = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+
+btn.forEach((btn) => {
+    console.log(btn);
+    btn.addEventListener('click' ,(e) => {
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }else if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;
+        }else if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }else if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
